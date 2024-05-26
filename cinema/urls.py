3da11f0601ee67895232156f1ac27cfdf0223c27
kekW <panicua12@gmail.com>
2024-05-26 21:57:04 +0300

    Solution to serializers task

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,25 @@
-# write urls here
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from cinema.views import (
+    MovieViewSet,
+    GenreViewSet,
+    ActorViewSet,
+    CinemaHallViewSet,
+    MovieSessionViewSet,
+)
+
+app_name = "cinema"
+router = DefaultRouter()
+
+router.register("movies", MovieViewSet, basename="movie")
+router.register("genres", GenreViewSet, basename="genre")
+router.register("actors", ActorViewSet, basename="actor")
+router.register("cinema_halls", CinemaHallViewSet, basename="cinema_hall")
+router.register(
+    "movie_sessions", MovieSessionViewSet, basename="movie_session"
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
